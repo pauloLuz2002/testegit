3e3acb86eb22e0adb74af9041f2e2eb6dd7d86d3
pauloLuz2002 <paulo.cezar622002@gmail.com>
2020-09-24 21:44:44 -0300

    Mudar Hello World para Guten Morgen

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace TesteGit {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Guten morgen!");
         }
     }
 }
